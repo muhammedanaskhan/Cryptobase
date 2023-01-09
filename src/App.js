bd00ebb91e1132bc0e392357bffcff3a7eec8ade
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-01-09 07:59:43 -0800

    Routes and their respective Components files made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Link, Route } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import {Navbar} from './Components';
+import {Navbar, HomePage, Exchanges, Cryptocurrencies, News, CryptoDetails} from './Components';
 
 function App() {
   return (
@@ -10,7 +10,27 @@ function App() {
         <Navbar/>
       </div>
       <div className="main">
-
+          <Layout>
+            <div className='routes'>
+              <Routes>
+                <Route exact path='/'>
+                  <HomePage/>
+                </Route>
+                <Route exact path='/exchanges'>
+                  <Exchanges/>
+                </Route>
+                <Route exact path='/cryptocurrencies'>
+                  <Cryptocurrencies/>
+                </Route>
+                <Route exact path='/crypto/:coinId'>
+                  <CryptoDetails/>
+                </Route>
+                <Route exact path='/news'>
+                  <News/>
+                </Route>
+              </Routes>
+            </div>
+          </Layout>
       </div>
       <div className="footer">
 
