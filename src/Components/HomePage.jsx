cd72f03f6333aa01d83851a1b2e734ebe513f563
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-01-14 10:28:51 -0800

    Display cryptocurrencies data in cards under cryptocurrencies section

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import millify from 'millify'  //package to format the numbers
 import { Typography, Row, Col, Statistic } from 'antd';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {Cryptocurrencies, News } from '../Components'
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
@@ -12,7 +13,6 @@ function HomePage() {
   const { data, isFetching } = useGetCryptosQuery();
   const globalStats = data?.data?.stats;
 
-  console.log(data)
 
   if(isFetching) return 'Loading...'
 
@@ -27,8 +27,26 @@ function HomePage() {
                 <Col span={12}> <Statistic title='Total Market Cap' value={millify(globalStats.totalMarketCap)}></Statistic> </Col>
                 <Col span={12}> <Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)}></Statistic> </Col>
                 <Col span={12}> <Statistic title='Total Markets' value={millify(globalStats.totalMarkets)}></Statistic> </Col>
-
-              </Row>
+            </Row>
+            <div className='home-heading-container'>
+              <Title level={2} className="home-title">
+                Top 10 Cryptocurrencies in the world
+              </Title>
+              <Title level={3} className="show-more">
+                <Link to='/cryptocurrencies'>Show More</Link>
+              </Title>
+            </div>
+            <Cryptocurrencies simplified/>
+            <div className='home-heading-container'>
+              <Title level={2} className="home-title">
+                Latest crypto News
+              </Title>
+              <Title level={3} className="show-more">
+                <Link to='/news'>Show More</Link>
+              </Title>
+            </div>
+            <News simplified />
+      
         </>
   )
 }
