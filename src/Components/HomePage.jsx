1053b26a5d0485d9929186c8d9a9cf31f8e41896
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-01-14 08:06:43 -0800

    Glabal stats data made presentable with millify

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-//import millify from 'millify'  //package to format the numbers
+import millify from 'millify'  //package to format the numbers
 import { Typography, Row, Col, Statistic } from 'antd';
 //import { Link } from 'react-router-dom';
 
@@ -10,20 +10,23 @@ const {Title} = Typography;  // destructuring <Typography.Title/>
 function HomePage() {
 
   const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
 
   console.log(data)
 
-  
+  if(isFetching) return 'Loading...'
+
+
 
   return (
         <>
             <Title level={2} className='heading'>Global Crypto Stats</Title>
             <Row>
-                <Col span={12}> <Statistic title='Total Cryptocurrencies' value="5"></Statistic> </Col>
-                <Col span={12}> <Statistic title='Total Exchanges' value="5"></Statistic> </Col>
-                <Col span={12}> <Statistic title='Total Market Cap' value="5"></Statistic> </Col>
-                <Col span={12}> <Statistic title='Total 24h Volume' value="5"></Statistic> </Col>
-                <Col span={12}> <Statistic title='Total Markets' value="5"></Statistic> </Col>
+                <Col span={12}> <Statistic title='Total Cryptocurrencies' value={millify(globalStats.total)}></Statistic> </Col>
+                <Col span={12}> <Statistic title='Total Exchanges' value={globalStats.totalExchanges}></Statistic> </Col>
+                <Col span={12}> <Statistic title='Total Market Cap' value={millify(globalStats.totalMarketCap)}></Statistic> </Col>
+                <Col span={12}> <Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)}></Statistic> </Col>
+                <Col span={12}> <Statistic title='Total Markets' value={millify(globalStats.totalMarkets)}></Statistic> </Col>
 
               </Row>
         </>
