286637a06fe777f5cef953a7dee0bab8aaf8246a
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-01-17 10:53:22 -0800

    Fetching coin's data inside cryptoDetails page via useParams() implemented

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
                 <Route exact path='/' element={<HomePage/>}></Route>
                 <Route exact path='/exchanges' element={<Exchanges/>}></Route>
                 <Route exact path='cryptocurrencies' element={<Cryptocurrencies/>}></Route>
+                <Route exact path='/crypto/:coinId' element={<CryptoDetails/>}></Route>
                 <Route exact path='cryptodetails' element={<CryptoDetails/>}></Route>
                 <Route exact path='news' element={<News/>}></Route>
               </Routes>
